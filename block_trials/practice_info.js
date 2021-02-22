11913c39013f7e94f0bd739f7bb2534ddfdd46f5
Your Name <you@example.com>
2021-02-22 16:48:08 +0200

    Converted images to jpg to speed up loading. Minor changes to instructions after beta-testing.

--- block_trials/practice_info.js
+++ block_trials/practice_info.js
@@ -2,82 +2,82 @@
 var practice_info = [
   {
     "emotion_type": "neg",
-    "emotion_image": "IAPS/neg4.bmp",
+    "emotion_image": "img/ESST/IAPS/neg4.jpg",
     "emotion_condition": "neg4",
     "condition": "go",
     "arrow_direction": "left",
     "arrow_key_correct": "c",
-    "arrow_image": "signals/left_stim.bmp",
+    "arrow_image": "img/ESST/signals/left_stim.bmp",
     "trial_response_correct": "l"
   },
   {
     "emotion_type": "neg",
-    "emotion_image": "IAPS/neg2_m.bmp",
+    "emotion_image": "img/ESST/IAPS/neg2_m.jpg",
     "emotion_condition": "neg2_m",
     "condition": "go",
     "arrow_direction": "right",
     "arrow_key_correct": "n",
-    "arrow_image": "signals/right_stim.bmp",
+    "arrow_image": "img/ESST/signals/right_stim.bmp",
     "trial_response_correct": "r"
   },
   {
     "emotion_type": "neu",
-    "emotion_image": "IAPS/neutral9_m.bmp",
+    "emotion_image": "img/ESST/IAPS/neutral9_m.jpg",
     "emotion_condition": "neutral9_m",
     "condition": "stop",
     "arrow_direction": "left",
     "arrow_key_correct": "c",
-    "arrow_image": "signals/left_stim.bmp",
+    "arrow_image": "img/ESST/signals/left_stim.bmp",
     "trial_response_correct": "x"
   },
   {
     "emotion_type": "neu",
-    "emotion_image": "IAPS/neutral27_m.bmp",
+    "emotion_image": "img/ESST/IAPS/neutral27_m.jpg",
     "emotion_condition": "neutral27_m",
     "condition": "stop",
     "arrow_direction": "right",
     "arrow_key_correct": "n",
-    "arrow_image": "signals/right_stim.bmp",
+    "arrow_image": "img/ESST/signals/right_stim.bmp",
     "trial_response_correct": "x"
   },
   {
     "emotion_type": "neg",
-    "emotion_image": "IAPS/neg4.bmp",
+    "emotion_image": "img/ESST/IAPS/neg4.jpg",
     "emotion_condition": "neg4",
     "condition": "go",
     "arrow_direction": "left",
     "arrow_key_correct": "c",
-    "arrow_image": "signals/left_stim.bmp",
+    "arrow_image": "img/ESST/signals/left_stim.bmp",
     "trial_response_correct": "l"
   },
   {
     "emotion_type": "neg",
-    "emotion_image": "IAPS/neg2_m.bmp",
+    "emotion_image": "img/ESST/IAPS/neg2_m.jpg",
     "emotion_condition": "neg2_m",
     "condition": "go",
     "arrow_direction": "right",
     "arrow_key_correct": "n",
-    "arrow_image": "signals/right_stim.bmp",
+    "arrow_image": "img/ESST/signals/right_stim.bmp",
     "trial_response_correct": "r"
   },
   {
     "emotion_type": "neu",
-    "emotion_image": "IAPS/neutral9_m.bmp",
+    "emotion_image": "img/ESST/IAPS/neutral9_m.jpg",
     "emotion_condition": "neutral9_m",
     "condition": "stop",
     "arrow_direction": "left",
     "arrow_key_correct": "c",
-    "arrow_image": "signals/left_stim.bmp",
+    "arrow_image": "img/ESST/signals/left_stim.bmp",
     "trial_response_correct": "x"
   },
   {
     "emotion_type": "neu",
-    "emotion_image": "IAPS/neutral27_m.bmp",
+    "emotion_image": "img/ESST/IAPS/neutral27_m.jpg",
     "emotion_condition": "neutral27_m",
     "condition": "stop",
     "arrow_direction": "right",
     "arrow_key_correct": "n",
-    "arrow_image": "signals/right_stim.bmp",
+    "arrow_image": "img/ESST/signals/right_stim.bmp",
     "trial_response_correct": "x"
   }
 ];
